1a1fbae1022aa941237d3ebf6c15d3a7b21ecd67
TadeoKi <tadeo.ki@correo.ucu.edu.uy>
2023-04-16 20:29:31 -0300

    lalala

---
+++ Ejercicio 1/src/Library/Estante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace SRP
+{
+    public class Estante
+    {
+        ArrayList ListaLibros = new ArrayList();
+        public string LibraryShelve { get ; set; }
+        
+        public Estante(String shelve)
+        {
+            this.LibraryShelve = shelve; 
+
+        }
+
+        public void AddBook(Book book)
+        {
+            ListaLibros.Add(book);
+            Console.WriteLine($"Se agrego el libro {book} al estante {this.LibraryShelve}");
+        }
+
+        public override string ToString()
+        {
+            return $"{this.LibraryShelve}";
+        }
+        
+    }
+}
